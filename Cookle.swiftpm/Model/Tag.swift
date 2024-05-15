16c3f1bebe852bf14bc229cb4fd1c986771a0a13
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2024-05-16 08:47:08 +0900

    Add descriptor to Tag protocol

--- Cookle.swiftpm/Model/Tag.swift
+++ Cookle.swiftpm/Model/Tag.swift
@@ -10,6 +10,7 @@ import SwiftData
 protocol Tag: PersistentModel {
     var value: String { get }
     var recipes: [Recipe] { get }
+    static var descriptor: FetchDescriptor<Self> { get }
     static func create(context: ModelContext, value: String) -> Self
     func update(value: String)
 }
