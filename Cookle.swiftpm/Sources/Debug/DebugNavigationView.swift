8fd461fe37de4450bcbf06e8c8ef541aad6a483b
muhiro_ <muhiro.12@gmail.com>
2024-05-23 10:03:05 +0900

    Modify DebugRootView

--- Cookle.swiftpm/Sources/Debug/DebugNavigationView.swift
+++ Cookle.swiftpm/Sources/Debug/DebugNavigationView.swift
@@ -6,47 +6,14 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct DebugNavigationView: View {
-    @Environment(\.modelContext) private var context
-
-    @Query(Diary.descriptor) private var diaries: [Diary]
-    @Query(DiaryObject.descriptor) private var diaryObjects: [DiaryObject]
-    @Query(Recipe.descriptor) private var recipes: [Recipe]
-    @Query(IngredientObject.descriptor) private var ingredientObjects: [IngredientObject]
-    @Query(Ingredient.descriptor) private var ingredients: [Ingredient]
-    @Query(Category.descriptor) private var categories: [Category]
-
     @State private var content: DebugContent?
     @State private var detail: Int?
 
     var body: some View {
         NavigationSplitView {
             DebugRootView(selection: $content)
-                .toolbar {
-                    ToolbarItem {
-                        Button("Delete All", systemImage: "trash") {
-                            withAnimation {
-                                diaries.forEach { context.delete($0) }
-                                diaryObjects.forEach { context.delete($0) }
-                                recipes.forEach { context.delete($0) }
-                                ingredients.forEach { context.delete($0) }
-                                ingredientObjects.forEach { context.delete($0) }
-                                categories.forEach { context.delete($0) }
-                            }
-                        }
-                    }
-                    ToolbarItem {
-                        Button("Add Random Diary", systemImage: "dice") {
-                            withAnimation {
-                                _ = ModelContainerPreview { _ in
-                                    EmptyView()
-                                }.randomDiary(context)
-                            }
-                        }
-                    }
-                }
                 .navigationTitle("Debug")
         } content: {
             if let content {
--- Cookle.swiftpm/Sources/Debug/DebugRootView.swift
+++ Cookle.swiftpm/Sources/Debug/DebugRootView.swift
@@ -1,8 +1,18 @@
 import SwiftUI
+import SwiftData
 
 struct DebugRootView: View {
+    @Environment(\.modelContext) private var context
+
     @AppStorage(.isICloudOn) private var isICloudOn
     @AppStorage(.isDebugOn) private var isDebugOn
+
+    @Query(Diary.descriptor) private var diaries: [Diary]
+    @Query(DiaryObject.descriptor) private var diaryObjects: [DiaryObject]
+    @Query(Recipe.descriptor) private var recipes: [Recipe]
+    @Query(IngredientObject.descriptor) private var ingredientObjects: [IngredientObject]
+    @Query(Ingredient.descriptor) private var ingredients: [Ingredient]
+    @Query(Category.descriptor) private var categories: [Category]
 
     @Binding private var selection: DebugContent?
     
@@ -33,6 +43,29 @@ struct DebugRootView: View {
             Section {
                 Toggle("iCloud On", isOn: $isICloudOn)
                 Toggle("Debug On", isOn: $isDebugOn)
+            }
+        }
+        .toolbar {
+            ToolbarItem {
+                Button("Delete All", systemImage: "trash") {
+                    withAnimation {
+                        diaries.forEach { context.delete($0) }
+                        diaryObjects.forEach { context.delete($0) }
+                        recipes.forEach { context.delete($0) }
+                        ingredients.forEach { context.delete($0) }
+                        ingredientObjects.forEach { context.delete($0) }
+                        categories.forEach { context.delete($0) }
+                    }
+                }
+            }
+            ToolbarItem {
+                Button("Add Random Diary", systemImage: "dice") {
+                    withAnimation {
+                        _ = ModelContainerPreview { _ in
+                            EmptyView()
+                        }.randomDiary(context)
+                    }
+                }
             }
         }
     }
