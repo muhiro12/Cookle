cfa9e06ae25b8d31512c97f562a00f56ad9f43fd
muhiro_ <muhiro.12@gmail.com>
2024-05-26 23:11:43 +0900

    Show DeleteButton

--- Cookle.swiftpm/Sources/Diary/Component/DeleteDiaryButton.swift
+++ Cookle.swiftpm/Sources/Diary/Component/DeleteDiaryButton.swift
@@ -17,7 +17,6 @@ struct DeleteDiaryButton: View {
         } message: {
             Text("Are you sure you want to delete this item? This action cannot be undone.")
         }
-        .hidden() // TODO: Show
     }
 }
 
--- Cookle.swiftpm/Sources/Recipe/Component/DeleteRecipeButton.swift
+++ Cookle.swiftpm/Sources/Recipe/Component/DeleteRecipeButton.swift
@@ -17,7 +17,6 @@ struct DeleteRecipeButton: View {
         } message: {
             Text("Are you sure you want to delete this item? This action cannot be undone.")
         }
-        .hidden() // TODO: Show
     }
 }
 
