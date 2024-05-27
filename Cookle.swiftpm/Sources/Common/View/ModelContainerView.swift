b6fd3b7382d01e235047a9003fc8ea0d3abcc2b4
muhiro_ <muhiro.12@gmail.com>
2024-05-27 18:45:37 +0900

    Organize ContentView

---
+++ Cookle.swiftpm/Sources/Common/View/ModelContainerView.swift
@@ -0,0 +1,33 @@
+//
+//  ModelContainerView.swift
+//  
+//
+//  Created by Hiromu Nakano on 2024/05/27.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ModelContainerView: View {
+    @AppStorage(.isICloudOn) private var isICloudOn
+
+    private var sharedModelContainer: ModelContainer!
+
+    init() {
+        sharedModelContainer = try! .init(
+            for: Recipe.self,
+            configurations: .init(
+                cloudKitDatabase: isICloudOn ? .automatic : .none
+            )
+        )
+    }
+
+    var body: some View {
+        MainTabView()
+            .modelContainer(sharedModelContainer)
+    }
+}
+
+#Preview {
+    ModelContainerView()
+}
--- Cookle.swiftpm/Sources/ContentView.swift
+++ Cookle.swiftpm/Sources/ContentView.swift
@@ -6,61 +6,12 @@
 //
 
 import SwiftUI
-import SwiftData
 
 public struct ContentView: View {
-    @AppStorage(.isICloudOn) private var isICloudOn
-    @AppStorage(.isDebugOn) private var isDebugOn
-
-    private var sharedModelContainer: ModelContainer!
-    private var sharedTabController: TabController!
-
-    public init() {
-        sharedModelContainer = try! .init(
-            for: Recipe.self,
-            configurations: .init(
-                cloudKitDatabase: isICloudOn ? .automatic : .none
-            )
-        )
-
-        sharedTabController = .init(initialTab: .diary)
-
-        // TODO: Remove
-        isDebugOn = true
-    }
+    public init() {}
 
     public var body: some View {
-        TabView(selection: sharedTabController.selection) {
-            DiaryNavigationView()
-                .tabItem {
-                    Label("Diary", systemImage: "book")
-                }
-                .tag(Tab.diary)
-            RecipeNavigationView()
-                .tabItem {
-                    Label("Recipe", systemImage: "book.pages")
-                }
-                .tag(Tab.recipe)
-            TagNavigationView<Ingredient>()
-                .tabItem {
-                    Label("Ingredient", systemImage: "refrigerator")
-                }
-                .tag(Tab.ingredient)
-            TagNavigationView<Category>()
-                .tabItem {
-                    Label("Category", systemImage: "frying.pan")
-                }
-                .tag(Tab.category)
-            if isDebugOn {
-                DebugNavigationView()
-                    .tabItem {
-                        Label("Debug", systemImage: "flask")
-                    }
-                    .tag(Tab.debug)
-            }
-        }
-        .modelContainer(sharedModelContainer)
-        .environment(sharedTabController)
+        ModelContainerView()
     }
 }
 
