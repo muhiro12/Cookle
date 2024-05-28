e077cab76c71cedcb911ca4538ec92d1da536f3c
muhiro_ <muhiro.12@gmail.com>
2024-05-28 09:59:02 +0900

    Modify MainNavigation

--- Cookle.swiftpm/Sources/Common/View/MainNavigationView.swift
+++ Cookle.swiftpm/Sources/Common/View/MainNavigationView.swift
@@ -11,12 +11,13 @@ struct MainNavigationView: View {
     @AppStorage(.isDebugOn) private var isDebugOn
 
     @State private var content: Tab?
-    @State private var detail: (any Hashable)?
+    @State private var item: AnyHashable?
+    @State private var detail: Recipe?
 
     @State private var isDebugPresented = false
 
     var body: some View {
-        NavigationSplitView {
+        NavigationSplitView() {
             List(selection: $content) {
                 Label("Diary", systemImage: "book")
                     .tag(Tab.diary)
@@ -38,78 +39,96 @@ struct MainNavigationView: View {
             }
             .navigationTitle("Cookle")
         } content: {
-            switch content {
-            case .diary:
-                DiaryListView(selection: .init(
-                    get: { detail as? Diary },
-                    set: { detail = $0 }
-                ))
-                .toolbar {
-                    ToolbarItem {
-                        AddDiaryButton()
-                    }
-                    ToolbarItem {
-                        AddRecipeButton()
+            NavigationStack {
+                Group {
+                    switch content {
+                    case .diary:
+                        DiaryListView(selection: .init(
+                            get: { item as? Diary },
+                            set: { item = $0 }
+                        ))
+                        .toolbar {
+                            ToolbarItem {
+                                AddDiaryButton()
+                            }
+                            ToolbarItem {
+                                AddRecipeButton()
+                            }
+                        }
+                        .navigationTitle("Diary")
+                    case .recipe:
+                        RecipeListView(selection: $detail)
+                            .toolbar {
+                                ToolbarItem {
+                                    AddRecipeButton()
+                                }
+                            }
+                            .navigationTitle("Recipe")
+                    case .ingredient:
+                        TagListView(selection: .init(
+                            get: { item as? Ingredient },
+                            set: { item = $0 }
+                        ))
+                        .toolbar {
+                            ToolbarItem {
+                                AddRecipeButton()
+                            }
+                        }
+                        .navigationTitle("Ingredient")
+                    case .category:
+                        TagListView(selection: .init(
+                            get: { item as? Category },
+                            set: { item = $0 }
+                        ))
+                        .toolbar {
+                            ToolbarItem {
+                                AddRecipeButton()
+                            }
+                        }
+                        .navigationTitle("Category")
+                    default:
+                        EmptyView()
                     }
                 }
-                .navigationTitle("Diary")
-            case .recipe:
-                RecipeListView(selection: .init(
-                    get: { detail as? Recipe },
-                    set: { detail = $0 }
-                ))
-                .toolbar {
-                    ToolbarItem {
-                        AddRecipeButton()
+                .navigationDestination(item: $item) { item in
+                    switch item {
+                    case let diary as Diary:
+                        DiaryView(selection: $detail)
+                            .toolbar {
+                                ToolbarItem(placement: .destructiveAction) {
+                                    DeleteDiaryButton()
+                                }
+                                ToolbarItem(placement: .confirmationAction) {
+                                    EditDiaryButton()
+                                }
+                            }
+                            .navigationTitle(diary.date.formatted(.dateTime.year().month().day()))
+                            .environment(diary)
+                    case let ingredient as Ingredient:
+                        TagView<Ingredient>(selection: $detail)
+                            .toolbar {
+                                ToolbarItem {
+                                    EditTagButton<Ingredient>()
+                                }
+                            }
+                            .navigationTitle(ingredient.value)
+                            .environment(ingredient)
+                    case let category as Category:
+                        TagView<Category>(selection: $detail)
+                            .toolbar {
+                                ToolbarItem {
+                                    EditTagButton<Category>()
+                                }
+                            }
+                            .navigationTitle(category.value)
+                            .environment(category)
+                    default:
+                        EmptyView()
                     }
                 }
-                .navigationTitle("Recipe")
-            case .ingredient:
-                TagListView(selection: .init(
-                    get: { detail as? Ingredient },
-                    set: { detail = $0 }
-                ))
-                .toolbar {
-                    ToolbarItem {
-                        AddRecipeButton()
-                    }
-                }
-                .navigationTitle("Ingredient")
-            case .category:
-                TagListView(selection: .init(
-                    get: { detail as? Category },
-                    set: { detail = $0 }
-                ))
-                .toolbar {
-                    ToolbarItem {
-                        AddRecipeButton()
-                    }
-                }
-                .navigationTitle("Category")
-            default:
-                EmptyView()
             }
         } detail: {
-            switch detail {
-            case let diary as Diary:
-                DiaryView(selection: .init(
-                    get: { detail as? Recipe },
-                    set: {
-                        content = .recipe
-                        detail = $0
-                    }
-                ))
-                .toolbar {
-                    ToolbarItem(placement: .destructiveAction) {
-                        DeleteDiaryButton()
-                    }
-                    ToolbarItem(placement: .confirmationAction) {
-                        EditDiaryButton()
-                    }
-                }
-                .navigationTitle(diary.date.formatted(.dateTime.year().month().day()))
-                .environment(diary)
-            case let recipe as Recipe:
+            if let detail {
                 RecipeView()
                     .toolbar {
                         ToolbarItem(placement: .destructiveAction) {
@@ -119,40 +138,8 @@ struct MainNavigationView: View {
                             EditRecipeButton()
                         }
                     }
-                    .navigationTitle(recipe.name)
-                    .environment(recipe)
-            case let ingredient as Ingredient:
-                TagView<Ingredient>(selection: .init(
-                    get: { detail as? Recipe },
-                    set: {
-                        content = .recipe
-                        detail = $0
-                    }
-                ))
-                .toolbar {
-                    ToolbarItem {
-                        EditTagButton<Ingredient>()
-                    }
-                }
-                .navigationTitle(ingredient.value)
-                .environment(ingredient)
-            case let category as Category:
-                TagView<Category>(selection: .init(
-                    get: { detail as? Recipe },
-                    set: {
-                        content = .recipe
-                        detail = $0
-                    }
-                ))
-                .toolbar {
-                    ToolbarItem {
-                        EditTagButton<Category>()
-                    }
-                }
-                .navigationTitle(category.value)
-                .environment(category)
-            default:
-                EmptyView()
+                    .navigationTitle(detail.name)
+                    .environment(detail)
             }
         }
         .sheet(isPresented: $isDebugPresented) {
