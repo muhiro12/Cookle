79890161d4b9cfc22ed93a867b90c45cb54b0102
muhiro_ <muhiro.12@gmail.com>
2024-05-29 14:55:32 +0900

    Add timestamp to Tag

--- Cookle.swiftpm/Sources/Tag/Model/Tag.swift
+++ Cookle.swiftpm/Sources/Tag/Model/Tag.swift
@@ -5,11 +5,14 @@
 //  Created by Hiromu Nakano on 2024/04/14.
 //
 
+import Foundation
 import SwiftData
 
 protocol Tag: PersistentModel {
     var value: String! { get }
     var recipes: [Recipe]! { get }
+    var createdTimestamp: Date! { get }
+    var modifiedTimestamp: Date! { get }
     static var descriptor: FetchDescriptor<Self> { get }
     static func create(context: ModelContext, value: String) -> Self
     func update(value: String)
