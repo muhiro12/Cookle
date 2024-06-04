8e4810def4bc63b182bd36c48dd9fbaefe936d4e
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2024-06-04 21:54:26 +0900

    Add LicenseView

---
+++ Cookle.swiftpm/Sources/Package/LicensePlist/LicenseView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import WebKit
+
+struct LicenseView: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        let webView = WKWebView()
+        if let url = Bundle.main.url(forResource: "License", withExtension: "html") {
+            webView.load(.init(url: url))
+        }
+        return webView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
+#Preview {
+    LicenseView()
+}
