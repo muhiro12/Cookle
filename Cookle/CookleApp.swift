5d1420b9bf49a17b11b1ad895cd7fde8571c0166
muhiro_ <muhiro.12@gmail.com>
2025-05-02 20:43:14 +0900

    Refactor project directory structure

---
+++ Cookle/CookleApp.swift
@@ -0,0 +1,78 @@
+//
+//  CookleApp.swift
+//  Cookle
+//
+//  Created by Hiromu Nakano on 2024/05/20.
+//
+
+import AppIntents
+import GoogleMobileAdsWrapper
+import LicenseListWrapper
+import StoreKitWrapper
+import SwiftUI
+
+@main
+struct CookleApp: App {
+    @AppStorage(.isSubscribeOn) private var isSubscribeOn
+    @AppStorage(.isICloudOn) private var isICloudOn
+    @AppStorage(.isDebugOn) private var isDebugOn
+
+    private let sharedGoogleMobileAdsController: GoogleMobileAdsController
+    private let sharedStore: Store
+
+    init() {
+        sharedGoogleMobileAdsController = .init(
+            adUnitID: {
+                #if DEBUG
+                Secret.adUnitIDDev
+                #else
+                Secret.adUnitID
+                #endif
+            }()
+        )
+
+        sharedStore = .init()
+
+        CookleShortcuts.updateAppShortcutParameters()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .cookleEnvironment(
+                    googleMobileAds: {
+                        sharedGoogleMobileAdsController.buildNativeAd($0)
+                    },
+                    licenseList: {
+                        LicenseListView()
+                    },
+                    storeKit: {
+                        sharedStore.buildSubscriptionSection()
+                    },
+                    appIntents: {
+                        ShortcutsLink()
+                            .shortcutsLinkStyle(.automaticOutline)
+                    }
+                )
+                .task {
+                    #if DEBUG
+                    isDebugOn = true
+                    #endif
+
+                    sharedGoogleMobileAdsController.start()
+
+                    sharedStore.open(
+                        groupID: Secret.groupID,
+                        productIDs: [Secret.productID]
+                    ) {
+                        isSubscribeOn = $0.contains {
+                            $0.id == Secret.productID
+                        }
+                        if !isSubscribeOn {
+                            isICloudOn = false
+                        }
+                    }
+                }
+        }
+    }
+}
---
+++ Cookle/Sources/Common/Model/CookleImagePlayground.swift
@@ -0,0 +1,62 @@
+//
+//  CookleImagePlayground.swift
+//  Cookle Playgrounds
+//
+//  Created by Hiromu Nakano on 2025/03/30.
+//
+
+import ImagePlayground
+import SwiftUI
+
+enum CookleImagePlayground {
+    static var isSupported: Bool {
+        if #available(iOS 18.1, *) {
+            EnvironmentValues().supportsImagePlayground
+        } else {
+            false
+        }
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func cookleImagePlayground(
+        isPresented: Binding<Bool>,
+        recipe: Recipe?,
+        onCompletion: @escaping (Data) -> Void,
+        onCancellation: (() -> Void)? = nil
+    ) -> some View {
+        if #available(iOS 18.1, *) {
+            imagePlaygroundSheet(
+                isPresented: isPresented,
+                concepts: {
+                    var concepts = [ImagePlaygroundConcept].empty
+                    guard let recipe else {
+                        return concepts
+                    }
+                    concepts.append(
+                        .text(recipe.name)
+                    )
+                    recipe.ingredients?.forEach { ingredient in
+                        concepts.append(
+                            .text(ingredient.value)
+                        )
+                    }
+                    recipe.steps.forEach { step in
+                        concepts.append(
+                            .extracted(from: step, title: recipe.name)
+                        )
+                    }
+                    return concepts
+                }()
+            ) { url in
+                guard let data = try? Data(contentsOf: url) else {
+                    return
+                }
+                onCompletion(data)
+            } onCancellation: {
+                onCancellation?()
+            }
+        }
+    }
+}
