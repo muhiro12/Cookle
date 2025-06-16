c213bb1b0488334de87c07db72d7c3dadd66dec3
muhiro_ <muhiro.12@gmail.com>
2025-06-17 08:27:27 +0900

    Annotate RecipeEntityQuery methods with @MainActor for AppIntent thread safety

--- Cookle/Sources/AppIntent/Model/RecipeEntityQuery.swift
+++ Cookle/Sources/AppIntent/Model/RecipeEntityQuery.swift
@@ -2,6 +2,7 @@ import AppIntents
 import SwiftData
 
 struct RecipeEntityQuery: EntityStringQuery {
+    @MainActor
     func entities(for identifiers: [RecipeEntity.ID]) throws -> [RecipeEntity] {
         try identifiers.compactMap { id in
             let persistentIdentifier = try PersistentIdentifier(base64Encoded: id)
@@ -12,12 +13,14 @@ struct RecipeEntityQuery: EntityStringQuery {
         }
     }
 
+    @MainActor
     func entities(matching string: String) throws -> [RecipeEntity] {
         try CookleIntents.context.fetch(
             .recipes(.nameContains(string))
         ).compactMap(RecipeEntity.init)
     }
 
+    @MainActor
     func suggestedEntities() throws -> [RecipeEntity] {
         try CookleIntents.context.fetch(
             .recipes(.all)
