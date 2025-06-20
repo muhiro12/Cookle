9eeed5d89991874e8b25575ce9d01642fd922330
muhiro_ <muhiro.12@gmail.com>
2025-06-20 09:12:57 +0900

    Add Test target

---
+++ CookleTests/CookleTests.swift
@@ -0,0 +1,14 @@
+//
+//  CookleTests.swift
+//  CookleTests
+//
+//  Created by Hiromu Nakano on 2025/06/20.
+//
+
+import Testing
+
+struct CookleTests {
+    @Test func example() throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
