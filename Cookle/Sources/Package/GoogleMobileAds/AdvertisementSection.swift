33a0ae084651eae86317861988277f6dbb09a40f
muhiro_ <muhiro.12@gmail.com>
2025-06-20 10:46:55 +0900

    Run SwiftLint

--- Cookle/Sources/Package/GoogleMobileAds/AdvertisementSection.swift
+++ Cookle/Sources/Package/GoogleMobileAds/AdvertisementSection.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2022 Hiromu Nakano. All rights reserved.
 //
 
-import SwiftUI
 import GoogleMobileAdsWrapper
+import SwiftUI
 
 struct AdvertisementSection {
     enum Size: String {
