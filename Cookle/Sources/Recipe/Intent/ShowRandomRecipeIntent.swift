ee3e96316cd1c1203e05c2130c5633bd4746e18a
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2025-06-20 16:24:46 +0900

    Refine intents to use explicit contexts

--- Cookle/Sources/Recipe/Intent/ShowRandomRecipeIntent.swift
+++ Cookle/Sources/Recipe/Intent/ShowRandomRecipeIntent.swift
@@ -6,6 +6,7 @@
 //
 
 import AppIntents
+import SwiftData
 import SwiftUI
 import SwiftUtilities
 
@@ -14,17 +15,19 @@ struct ShowRandomRecipeIntent: AppIntent, IntentPerformer {
         .init("Show Random Recipe")
     }
 
-    typealias Input = Void
+    typealias Input = ModelContext
     typealias Output = RecipeEntity?
 
+    @Dependency(\.modelContainer) private var modelContainer
+
     @MainActor
-    private static func recipe() throws -> Recipe? {
-        try CookleIntents.context.fetchRandom(.recipes(.all))
+    private static func recipe(context: ModelContext) throws -> Recipe? {
+        try context.fetchRandom(.recipes(.all))
     }
 
     @MainActor
-    static func perform(_: Input) throws -> Output {
-        guard let recipe = try recipe() else {
+    static func perform(_ input: Input) throws -> Output {
+        guard let recipe = try recipe(context: input) else {
             return nil
         }
         return RecipeEntity(recipe)
@@ -32,7 +35,7 @@ struct ShowRandomRecipeIntent: AppIntent, IntentPerformer {
 
     @MainActor
     func perform() throws -> some IntentResult & ProvidesDialog & ShowsSnippetView {
-        guard let recipe = try Self.recipe() else {
+        guard let recipe = try Self.recipe(context: modelContainer.mainContext) else {
             return .result(dialog: "Not Found")
         }
         return .result(dialog: .init(stringLiteral: recipe.name)) {
