82b7dbf7278e83c5a238be54e1c0defccd01bd47
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2025-08-09 07:51:58 +0900

    Fix speech recognition crash by main actor access

--- Cookle/Sources/Common/Models/SpeechRecognizer.swift
+++ Cookle/Sources/Common/Models/SpeechRecognizer.swift
@@ -25,8 +25,10 @@ final class SpeechRecognizer: NSObject, ObservableObject {
 
         let inputNode = audioEngine.inputNode
         let format = inputNode.outputFormat(forBus: .zero)
-        inputNode.installTap(onBus: .zero, bufferSize: 1_024, format: format) { buffer, _ in
-            request.append(buffer)
+        inputNode.installTap(onBus: .zero, bufferSize: 1_024, format: format) { [weak self] buffer, _ in
+            Task { @MainActor [buffer] in
+                self?.request?.append(buffer)
+            }
         }
 
         task = recognizer?.recognitionTask(with: request) { result, error in
