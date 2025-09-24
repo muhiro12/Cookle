429611d56ccbc6c7da9a8727558290c554fca357
muhiro_ <muhiro.12@gmail.com>
2025-09-25 00:51:46 +0900

    Add Widgets target (sources, assets, scheme) and update the Xcode project accordingly.

---
+++ Widgets/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Widgets
+//
+//  Created by Hiromu Nakano on 2025/09/25.
+//
+
+import AppIntents
+import WidgetKit
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by Hiromu Nakano on 2025/09/25.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsControl()
+        WidgetsLiveActivity()
+    }
+}
