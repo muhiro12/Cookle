2174ca0e77e46e975a9b229dbfd8b0ed46986154
muhiro_ <muhiro.12@gmail.com>
2026-02-06 23:16:18 +0900

    Fix backward compatibility for DiaryObjectType coding

--- CookleLibrary/Sources/Diary/DiaryObjectType.swift
+++ CookleLibrary/Sources/Diary/DiaryObjectType.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 /// Meal kinds used in a diary.
-public nonisolated enum DiaryObjectType: CaseIterable, Codable, Identifiable {
+public nonisolated enum DiaryObjectType: String, CaseIterable, Codable, Identifiable {
     case breakfast
     case lunch
     case dinner
@@ -28,5 +28,50 @@ public nonisolated enum DiaryObjectType: CaseIterable, Codable, Identifiable {
     /// Stable string identifier for this case.
     public var id: String {
         .init(describing: self)
+    }
+}
+
+extension DiaryObjectType {
+    private struct CodingKey: Swift.CodingKey {
+        let stringValue: String
+        let intValue: Int? = .none
+
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+
+        init?(intValue: Int) {
+            _ = intValue
+            return nil
+        }
+    }
+
+    public init(from decoder: any Decoder) throws {
+        if let container = try? decoder.singleValueContainer(),
+           let rawValue = try? container.decode(String.self),
+           let value = Self(rawValue: rawValue) {
+            self = value
+            return
+        }
+
+        let container = try decoder.container(keyedBy: CodingKey.self)
+        if container.allKeys.count == 1,
+           let firstKey = container.allKeys.first,
+           let value = Self(rawValue: firstKey.stringValue) {
+            self = value
+            return
+        }
+
+        throw DecodingError.dataCorrupted(
+            .init(
+                codingPath: decoder.codingPath,
+                debugDescription: "Unsupported DiaryObjectType payload"
+            )
+        )
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
     }
 }
