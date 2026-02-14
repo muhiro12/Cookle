5c176e04566cffaf29b8eb04aa2532e301bdd9f4
muhiro_ <muhiro.12@gmail.com>
2026-02-14 10:19:57 +0900

    Remove Live Activity from Widgets extension

--- Widgets/Sources/WidgetsBundle.swift
+++ Widgets/Sources/WidgetsBundle.swift
@@ -14,6 +14,5 @@ struct WidgetsBundle: WidgetBundle {
         CookleTodayDiaryWidget()
         CookleRandomRecipeWidget()
         CookleLastOpenedRecipeWidget()
-        CookleCookingLiveActivity()
     }
 }
