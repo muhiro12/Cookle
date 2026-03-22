1678425bdaa5a7824c77954528684f8be84630c3
muhiro_ <muhiro.12@gmail.com>
2026-03-22 12:43:56 +0900

    Pin MHPlatform to the 1.0 release revision

--- CookleLibrary/Package.swift
+++ CookleLibrary/Package.swift
@@ -23,7 +23,7 @@ let kPackage = Package(
         ),
         .package(
             url: "https://github.com/muhiro12/MHPlatform.git",
-            branch: "main"
+            revision: "083817e803431d33825a5878593d63e399053ed9"
         )
     ],
     targets: [
