9f7d1cd85aef663e2b27fd562bbd36f25ea09343
muhiro_ <muhiro.12@gmail.com>
2026-04-18 01:17:11 +0900

    Add watch app target to Cookle

---
+++ Watch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cookle
+//
+//  Created by Hiromu Nakano on 2026/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Watch/CookleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookleWatchApp.swift
+//  Cookle
+//
+//  Created by Hiromu Nakano on 2026/04/18.
+//
+
+import SwiftUI
+
+@main
+struct CookleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
